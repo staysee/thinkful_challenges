389be72c1f0cd88f81c9e3bd4951f02108527f5f
Stacey Sugiono <staceysugiono@gmail.com>
2018-01-16 18:20:17 -0800

    cannot get other functions to work. only word count is working so far

--- text_analyzer/app.js
+++ text_analyzer/app.js
@@ -2,22 +2,33 @@ function removePunctuation(text){
   return text.toLowerCase().replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"");
 }
 
-function countUniqueWords(){
+function countUniqueWords(words){
+  var uniqueWords = [];
 
+  //if word is not in the uniqueWords array, add it
+  for(var i = 0; i < words.length; i++){
+    if (uniqueWords.indexOf(words[i]) === -1){
+      uniqueWords.push(words[i]);
+    }
+  }
+  //length of array is how many unique words
+  return uniqueWords.length;
 }
 
 function averageWordLength(){
+  //take length of all words together and divide by number of words
 
 }
 
 function createTextReport(text){
   var wordBank = removePunctuation(text);
   var wordCount = wordBank.length;
-  var uniqeWords;
+  var uniqeWords = countUniqueWords(wordBank);
   var avgWordLength;
 
   var textReport = $('.js-text-report');
-  textReport.find('.js-word-count').text(wordCount);
+  textReport.find('.js-word-count').html(wordCount);
+  textReport.find('.js-unique-words').html(uniqueWords);
 
   textReport.removeClass('hidden');
 
