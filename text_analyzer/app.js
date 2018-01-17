0422303ed35902fab6f2101171079c455b4b24e7
Stacey Sugiono <staceysugiono@gmail.com>
2018-01-16 18:00:03 -0800

    word count is working

--- text_analyzer/app.js
+++ text_analyzer/app.js
@@ -1,1 +1,38 @@
-// your code here!
+function removePunctuation(text){
+  return text.toLowerCase().replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"");
+}
+
+function countUniqueWords(){
+
+}
+
+function averageWordLength(){
+
+}
+
+function createTextReport(text){
+  var wordBank = removePunctuation(text);
+  var wordCount = wordBank.length;
+  var uniqeWords;
+  var avgWordLength;
+
+  var textReport = $('.js-text-report');
+  textReport.find('.js-word-count').text(wordCount);
+
+  textReport.removeClass('hidden');
+
+
+}
+
+function handleFormSubmit(){
+  $('.js-text-form').submit(function(event){
+    event.preventDefault();
+    //get user's text from the form
+    var userText = $(event.currentTarget).find('#user-text').val();
+    //alert(userText);
+
+    createTextReport(removePunctuation(userText));
+  })
+}
+
+$(handleFormSubmit);
