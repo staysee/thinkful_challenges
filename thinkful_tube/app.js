bcae3116fc618ac15d95351ee5c47e0d8626a6a8
Stacey Sugiono <staceysugiono@gmail.com>
2018-02-15 12:53:19 -0800

    setting up files

---
+++ thinkful_tube/app.js
@@ -0,0 +1,17 @@
+var YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
+
+
+//pass following in the params object:
+// - part: 'snippet'
+// - key: your API key as a string
+// - q: your search term as a string
+
+//find thumbnail image URL
+// hint: items.snippet.thumbnails.medium.url
+//
+
+
+//requirements: accept a user search term
+// get JSNO from youtube API based on search term
+// display thumbnail image of the returned videos
+
