b6a0d8279aad5e52964709b7925f6522639de3f0
Stacey Sugiono <staceysugiono@gmail.com>
2018-02-20 15:40:19 -0800

    results show up

--- thinkful_tube/app.js
+++ thinkful_tube/app.js
@@ -1,23 +1,6 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 const API_KEY = 'AIzaSyCLeJQoGA1Zn8dKKofZo41swOloXIcUefg';
 
-
-
-
-//pass following in the params object:
-// - part: 'snippet'
-// - key: your API key as a string
-// - q: your search term as a string
-
-//find thumbnail image URL
-// hint: items.snippet.thumbnails.medium.url
-//
-
-
-//requirements: accept a user search term
-// get JSNO from youtube API based on search term
-// display thumbnail image of the returned videos
-
 function getDataFromApi (searchTerm, callback){
   const query = {
     part: 'snippet',
@@ -32,11 +15,18 @@ function getDataFromApi (searchTerm, callback){
 }
 
 function renderResult(result) {
-  console.log('result');
+  return `
+    <div>
+      <h2>${result.snippet.title}</h2>
+      <img src="${result.snippet.thumbnails.medium.url}">
+    </div>
+  `
 }
 
 function displayYouTubeSearchData(data){
-  const results = data.items;
+  const results = $.map(data.items, function(item, index) {
+    return renderResult(item)
+  })
   $('.js-search-results').html(results);
 }
 
