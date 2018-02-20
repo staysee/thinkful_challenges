2954ea902f643df08d5c6ce7822abd81fa4431fb
Stacey Sugiono <staceysugiono@gmail.com>
2018-02-20 13:03:04 -0800

    api accessed in console

--- thinkful_tube/app.js
+++ thinkful_tube/app.js
@@ -1,4 +1,7 @@
-var YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyCLeJQoGA1Zn8dKKofZo41swOloXIcUefg';
+
+
 
 
 //pass following in the params object:
@@ -15,3 +18,39 @@ var YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 // get JSNO from youtube API based on search term
 // display thumbnail image of the returned videos
 
+function getDataFromApi (searchTerm, callback){
+  const query = {
+    part: 'snippet',
+    key: API_KEY,
+    q: searchTerm,
+    per_page: 5,
+    type: 'video'
+  }
+
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+  console.log($.getJSON(YOUTUBE_SEARCH_URL, query, callback))
+}
+
+function renderResult(result) {
+  console.log('result');
+}
+
+function displayYouTubeSearchData(data){
+  const results = data.items;
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(event){
+    event.preventDefault();
+    var queryTarget = $(event.currentTarget).find('.js-query');
+    var query = queryTarget.val();
+    console.log(query);
+
+    queryTarget.val("");  //clear input
+    getDataFromApi(query, displayYouTubeSearchData)
+
+  })
+}
+
+$(watchSubmit);
