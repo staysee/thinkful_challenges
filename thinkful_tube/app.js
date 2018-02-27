0015c6c2fda933395d00fea4658547002bd0266b
Stacey Sugiono <staceysugiono@gmail.com>
2018-02-27 00:15:45 -0800

    lightbox somewhat works

--- thinkful_tube/app.js
+++ thinkful_tube/app.js
@@ -1,5 +1,90 @@
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'AIzaSyCLeJQoGA1Zn8dKKofZo41swOloXIcUefg';
+// load Youtube API code asynchronously
+var tag = document.createElement('script')
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+var isiOS = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)/i) != null //boolean check for iOS devices
+
+var youtubelightbox = document.getElementById('youtubelightbox')
+var player // variable to hold new YT.Player() instance
+
+// Hide lightbox when clicked on
+youtubelightbox.addEventListener('click', function(){
+    this.style.display = 'none'
+    player.stopVideo()
+}, false)
+
+// Exclude youtube iframe from above action
+youtubelightbox.querySelector('.centeredchild').addEventListener('click', function(e){
+    e.stopPropagation()
+}, false)
+
+
+// define onYouTubeIframeAPIReady() function and initialize lightbox when API is ready
+function onYouTubeIframeAPIReady() {
+    createlightbox()
+}
+
+// Extracts the Youtube video ID from a well formed Youtube URL
+function getyoutubeid(link){
+    // Assumed Youtube URL formats
+    // https://www.youtube.com/watch?v=Pe0jFDPHkzo
+    // https://youtu.be/Pe0jFDPHkzo
+    // https://www.youtube.com/v/Pe0jFDPHkzo
+    // and more
+
+    //See http://stackoverflow.com/a/6904504/4360074
+    var youtubeidreg = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+    return youtubeidreg.exec(link)[1] // return Youtube video ID portion of link
+}
+
+// Creates a new YT.Player() instance
+function createyoutubeplayer(videourl){
+    player = new YT.Player('playerdiv', {
+        videoId: videourl,
+        playerVars: {autoplay:1}
+    })
+}
+
+// Main Youtube lightbox function
+function createlightbox(){
+    var targetlinks = document.querySelectorAll('.lightbox')
+    for (var i=0; i<targetlinks.length; i++){
+        var link = targetlinks[i]
+        link._videoid = getyoutubeid(link) // store youtube video ID portion of link inside _videoid property
+        targetlinks[i].addEventListener('click', function(e){
+            youtubelightbox.style.display = 'block'
+            if (typeof player == 'undefined'){ // if video player hasn't been created yet
+                createyoutubeplayer(this._videoid)
+            }
+            else{
+                if (isiOS){ // iOS devices can only use the "cue" related methods
+                    player.cueVideoById(this._videoid)
+                }
+                else{
+                    player.loadVideoById(this._videoid)
+                }
+            }
+            e.preventDefault()
+        }, false)
+    }
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(event){
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    //console.log(query);
+
+    queryTarget.val("");  //clear input
+    getDataFromApi(query, displayYouTubeSearchData)
+  })
+}
+
+$(watchSubmit);
 
 function getDataFromApi (searchTerm, callback){
   const query = {
@@ -15,19 +100,15 @@ function getDataFromApi (searchTerm, callback){
 }
 
 function renderResult(result) {
-  const videoPreURL = 'https://youtube.com/watch?v=';
   const videoID = result.id.videoId;
-  const videoURL = videoPreURL + videoID;
-  const channelPreURL = 'https://youtube.com/channel/';
   const channelID = result.snippet.channelId;
-  const channelURL = channelPreURL + channelID;
 
   return `
     <div class="search-item">
-        <a class="video lightbox-trigger" href="${videoURL}" target="_blank"><img src="${result.snippet.thumbnails.medium.url}"></a>
+        <a class="video lightbox" href="https://youtube.com/watch?v=${videoID}" data-videoid=${videoID} onclick="createlightbox(); console.log(\'helloworld\'); return false"><img src="${result.snippet.thumbnails.medium.url}"></a>
         <div class="video-title">${result.snippet.title}</div>
         <div class="video-description">${result.snippet.description}</div>
-        <div class="channel-title">View more by <a href="${channelURL}" target="_blank">${result.snippet.channelTitle}</a></div>
+        <div class="channel-title">View more by <a href="https://youtube.com/channel/${channelID}" target="_blank">${result.snippet.channelTitle}</a></div>
     </div>
   `
 }
@@ -37,45 +118,7 @@ function displayYouTubeSearchData(data){
     return renderResult(item)
   })
   $('.js-search-results').html(results);
-  $('.total-results').html(data.pageInfo.totalResults);
+  // $('.total-results').html(data.pageInfo.totalResults);
 }
-
-function watchSubmit() {
-  $('.js-search-form').submit(function(event){
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
-    console.log(query);
-
-    queryTarget.val("");  //clear input
-    getDataFromApi(query, displayYouTubeSearchData)
-
-  })
-}
-
-function handleLightBox() {
-  $('.js-search-results').on('click', '.lightbox-trigger', function(event){
-    event.preventDefault()
-    var video_src = $(this).attr("href");
-
-    if ($('#lightbox').length > 0) {
-      $('.video-container').html('<iframe src="' + video_src +'""></iframe>')
-      $('#lightbox').show();
-    }
-    else {
-      var lightbox = `
-        <div id="lightbox">
-          <p>Click to close</p>
-          <div id="video-container">
-            <iframe src="${video_src}"></iframe>
-          </div>
-        </div>
-      `
-      $('body').append(lightbox);
-    }
-
-  })
-}
-
-$(watchSubmit);
-$(handleLightBox);
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyCLeJQoGA1Zn8dKKofZo41swOloXIcUefg';
